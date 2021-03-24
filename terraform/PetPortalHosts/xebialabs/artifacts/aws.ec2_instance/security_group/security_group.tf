35eddc1e904e98aa02c260096a272cdba6f5b59f
zvercodebender <rbroker@digital.ai>
2021-03-24 10:50:23 -0400

    Add security group

---
+++ terraform/PetPortalHosts/xebialabs/artifacts/aws.ec2_instance/security_group/security_group.tf
@@ -0,0 +1,63 @@
+##########################################################################
+resource "aws_security_group" "my_sg" {
+  name        = format("%s-security_group", var.project)
+  description = "Security group to demo terraform"
+
+  ingress {
+    description = "Allow HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTP on 8080"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "All in SG"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "tcp"
+    self        = true
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = format("%s-security_group", var.project)
+  }
+}
+###########################################################################
+output "security_group_id" {
+   value = aws_security_group.my_sg.id
+}
+output "security_group_name" {
+   value = aws_security_group.my_sg.name
+}
---
+++ terraform/PetPortalHosts/xebialabs/artifacts/aws.ec2_instance/security_group/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description  = "AWS Region"
+  type         = string
+  default      = "us-east-2"
+}
+
+variable "project" {
+  description = "Project Name"
+  type        = string
+  default     = "dad"
+}
